3bb137bb6988b61f7e3d1681ce2321c03940c01b
Guido Pola <guidopola@live.com>
2018-12-06 20:50:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Quick tutorial to translate.
+==================================
+For translating, you need to have git and the Qt Linguist tool
+[How to install and use git](https://help.github.com/articles/set-up-git/)
+[Download Qt Linguist](https://github.com/lelegard/qtlinguist-installers/releases)
+
+
+### Creating a translation
+To add a new language you need to use the [ISO 639-1](https://en.wikipedia.org/wiki/ISO_639-1) code as the file name, for example Portuguese (ISO 639-1 code "pt")
+
+Copy the en.ts and rename it to pt.ts
+
+    cp en.ts pt.ts
+
+Open the file with Qt Linguist to edit them
+
+    linguist pt.ts
+
+When you've finished create a pull request.
---
+++ es.ts
@@ -0,0 +1,327 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>ComponentItemModel</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="77"/>
+        <source>Net name</source>
+        <translation>Linea</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="77"/>
+        <source>Component</source>
+        <translation>Componente</translation>
+    </message>
+</context>
+<context>
+    <name>MainToolBar</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="179"/>
+        <location filename="../src/app/maintoolbar.cpp" line="459"/>
+        <source>Search...</source>
+        <translation>Buscar...</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="231"/>
+        <location filename="../src/app/maintoolbar.cpp" line="460"/>
+        <source>Component name</source>
+        <translation>Nombre del componente</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="235"/>
+        <location filename="../src/app/maintoolbar.cpp" line="243"/>
+        <location filename="../src/app/maintoolbar.cpp" line="463"/>
+        <source>Hide Components names</source>
+        <translation>Ocultar los nombres de los componentes</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="245"/>
+        <location filename="../src/app/maintoolbar.cpp" line="465"/>
+        <source>Show Components names</source>
+        <translation>Mostrar los nombres de los componentes</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="255"/>
+        <location filename="../src/app/maintoolbar.cpp" line="467"/>
+        <source>Pin name</source>
+        <translation>Nombre del contacto</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="258"/>
+        <location filename="../src/app/maintoolbar.cpp" line="266"/>
+        <location filename="../src/app/maintoolbar.cpp" line="469"/>
+        <source>Hide Pins names</source>
+        <translation>Ocultar nombres de los contactos</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="268"/>
+        <location filename="../src/app/maintoolbar.cpp" line="471"/>
+        <source>Show Pins names</source>
+        <translation>Mostrar nombres de los contactos</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="277"/>
+        <source>No Contact</source>
+        <translation>Sin contacto</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="281"/>
+        <location filename="../src/app/maintoolbar.cpp" line="289"/>
+        <location filename="../src/app/maintoolbar.cpp" line="475"/>
+        <source>Hide No Contact pins</source>
+        <translation>Ocultar pines sin contacto</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="291"/>
+        <location filename="../src/app/maintoolbar.cpp" line="477"/>
+        <source>Show No Contact pins</source>
+        <translation>Mostrar pines sin contacto</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="300"/>
+        <location filename="../src/app/maintoolbar.cpp" line="479"/>
+        <source>Ground</source>
+        <translation>Tierra</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="302"/>
+        <location filename="../src/app/maintoolbar.cpp" line="308"/>
+        <location filename="../src/app/maintoolbar.cpp" line="481"/>
+        <source>Hide Ground contacts</source>
+        <translation>Ocultar pines a tierra</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="310"/>
+        <location filename="../src/app/maintoolbar.cpp" line="483"/>
+        <source>Show Ground contacts</source>
+        <translation>Mostrar pines a tierra</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="320"/>
+        <location filename="../src/app/maintoolbar.cpp" line="485"/>
+        <source>Show / Hide A</source>
+        <translation>Mostrar / Ocultar lado A</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="337"/>
+        <location filename="../src/app/maintoolbar.cpp" line="486"/>
+        <source>Show / Hide B</source>
+        <translation>Mostrar / Ocultar lado B</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="356"/>
+        <location filename="../src/app/maintoolbar.cpp" line="487"/>
+        <source>Show both sides</source>
+        <translation>Mostrar ambos lados</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="370"/>
+        <location filename="../src/app/maintoolbar.cpp" line="488"/>
+        <source>Reset zoom</source>
+        <translation>Restablecer zoom</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="384"/>
+        <location filename="../src/app/maintoolbar.cpp" line="489"/>
+        <source>Zoom in</source>
+        <translation>Acercar</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="396"/>
+        <location filename="../src/app/maintoolbar.cpp" line="490"/>
+        <source>Zoom out</source>
+        <translation>Alejar</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="410"/>
+        <location filename="../src/app/maintoolbar.cpp" line="491"/>
+        <source>Reset rotation</source>
+        <translation>Restablecer rotacíon</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="422"/>
+        <location filename="../src/app/maintoolbar.cpp" line="492"/>
+        <source>Rotate right</source>
+        <translation>Rotar hacia la derecha</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="435"/>
+        <location filename="../src/app/maintoolbar.cpp" line="493"/>
+        <source>Rotate left</source>
+        <translation>Rotar hacia la izquierda</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="473"/>
+        <source>No contact</source>
+        <translation>Sin contacto</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="39"/>
+        <location filename="../src/app/mainwindow.cpp" line="242"/>
+        <source>Open BRD</source>
+        <translation>Abrir BRD</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="40"/>
+        <location filename="../src/app/mainwindow.cpp" line="243"/>
+        <source>Phones</source>
+        <translation>Telefonos</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="41"/>
+        <location filename="../src/app/mainwindow.cpp" line="244"/>
+        <source>Tablets</source>
+        <translation>Tabletas</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="54"/>
+        <source>Open Board file</source>
+        <translation>Abrir archivo .brd</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="54"/>
+        <source>BRD Files (*.brd)</source>
+        <translation>Archivo BRD (*.brd)</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="100"/>
+        <location filename="../src/app/mainwindow.cpp" line="245"/>
+        <source>Cursor: %1</source>
+        <translation>Cursor: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="104"/>
+        <location filename="../src/app/mainwindow.cpp" line="144"/>
+        <location filename="../src/app/mainwindow.cpp" line="246"/>
+        <source>Pin name: %1</source>
+        <translation>Pin: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="108"/>
+        <location filename="../src/app/mainwindow.cpp" line="145"/>
+        <location filename="../src/app/mainwindow.cpp" line="247"/>
+        <source>Net name: %1</source>
+        <translation>Linea: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="150"/>
+        <source>The pin have contact on both sides.</source>
+        <translation>El pin tiene contacto en ambos lados.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="152"/>
+        <source>The pin [%1] have contact on both sides.</source>
+        <translation>El pin [%1] tiene contacto en ambos lados.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="159"/>
+        <source>The pin have contact on side %1 only.</source>
+        <translation>El pin tiene contacto solo en el lado %1.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="161"/>
+        <source>The pin [%1] have contact on side %2 only.</source>
+        <translation>El pin [%1] tiene contacto solo en el lado %2.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="193"/>
+        <source>Update available!</source>
+        <translation>Nueva versión disponible!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="195"/>
+        <source>An update is available!
+Please go to the Phoneboard &lt;a href=&quot;https://phoneboard.co&quot;&gt;website&lt;/a&gt; to download.</source>
+        <translation>Una nueva versión está disponible!
+Por favor visita el &lt;a href=&quot;https://phoneboard.co&quot;&gt;sitio web&lt;/a&gt; de Phoneboard para descargarlo.</translation>
+    </message>
+</context>
+<context>
+    <name>OverlayProgress</name>
+    <message>
+        <location filename="../src/app/overlayprogress.cpp" line="65"/>
+        <source>Loading %1...</source>
+        <extracomment>This text appears upside the loading progress bar.</extracomment>
+        <translation>Cargando %1...</translation>
+    </message>
+</context>
+<context>
+    <name>OverlaySettings</name>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="56"/>
+        <location filename="../src/app/overlaysettings.cpp" line="323"/>
+        <source>Welcome to Phoneboard!</source>
+        <translation>Bienvenido a Phoneboard!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="68"/>
+        <location filename="../src/app/overlaysettings.cpp" line="324"/>
+        <source>Remember this software is &lt;b&gt;free&lt;/b&gt;. To report any issues go &lt;a href=&quot;https://github.com/phoneboard/phoneboard/issues&quot;&gt;here&lt;/a&gt;.&lt;br&gt;If you find the app useful, consider supporting me on &lt;a href=&quot;https://www.patreon.com/Phoneboard&quot;&gt;Patreon&lt;/a&gt; or donate on &lt;a href=&quot;https://www.paypal.me/phoneboardapp&quot;&gt;Paypal&lt;/a&gt;.Thank you!</source>
+        <translation>Recuerda esta aplicación es &lt;b&gt;gratuita&lt;/b&gt;. Para reportar algún problema visita &lt;a href=&quot;https://github.com/phoneboard/phoneboard/issues&quot;&gt;este sitio&lt;/a&gt; .&lt;br&gt;Si encuentras útil esta aplicación considera ayudarme en &lt;a href=&quot;https://www.patreon.com/Phoneboard&quot;&gt;Patreon&lt;/a&gt; o donando en &lt;a href=&quot;https://www.paypal.me/phoneboardapp&quot;&gt;Paypal&lt;/a&gt;. Gracias!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="90"/>
+        <location filename="../src/app/overlaysettings.cpp" line="319"/>
+        <source>Language</source>
+        <translation>Idioma</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="120"/>
+        <location filename="../src/app/overlaysettings.cpp" line="318"/>
+        <source>Split Board</source>
+        <translation>Separar lados</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="122"/>
+        <location filename="../src/app/overlaysettings.cpp" line="320"/>
+        <source>Horizontal</source>
+        <translation>Horizontalmente</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="123"/>
+        <location filename="../src/app/overlaysettings.cpp" line="321"/>
+        <source>Vertical</source>
+        <translation>Verticalmente</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="142"/>
+        <location filename="../src/app/overlaysettings.cpp" line="334"/>
+        <source>No Contact Color</source>
+        <translation>Color de pines sin contacto</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="144"/>
+        <location filename="../src/app/overlaysettings.cpp" line="196"/>
+        <location filename="../src/app/overlaysettings.cpp" line="247"/>
+        <location filename="../src/app/overlaysettings.cpp" line="335"/>
+        <location filename="../src/app/overlaysettings.cpp" line="338"/>
+        <location filename="../src/app/overlaysettings.cpp" line="341"/>
+        <source>Reset</source>
+        <translation>Restablecer</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="194"/>
+        <location filename="../src/app/overlaysettings.cpp" line="337"/>
+        <source>Ground Color</source>
+        <translation>Color de pines a tierra</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="245"/>
+        <location filename="../src/app/overlaysettings.cpp" line="340"/>
+        <source>Active Color</source>
+        <translation>Color del pin activo</translation>
+    </message>
+</context>
+<context>
+    <name>OverlayWelcome</name>
+    <message>
+        <location filename="../src/app/overlaywelcome.cpp" line="53"/>
+        <source>What&apos;s new in v%1!</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
