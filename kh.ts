4cd07f44818c6e2ebbafaa0d233c677132526764
ICFixSothy <doem_sothy@outlook.com>
2018-12-10 18:14:14 +0700

    include Cambodia language in Phone Board
    I want to add Cambodia language in Programs Phone Board

---
+++ kh.ts
@@ -0,0 +1,326 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>ម៉ូដែលធាតុផ្សំ</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="77"/>
+        <source>ឈ្មោះបច្ចេកទេស</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="77"/>
+        <source>ធាតុផ្សំ</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>របារឧបករណ៍មេ</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="179"/>
+        <location filename="../src/app/maintoolbar.cpp" line="459"/>
+        <source>ស្វែងរក ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="231"/>
+        <location filename="../src/app/maintoolbar.cpp" line="460"/>
+        <source>ឈ្មោះធាតុផ្សំ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="235"/>
+        <location filename="../src/app/maintoolbar.cpp" line="243"/>
+        <location filename="../src/app/maintoolbar.cpp" line="463"/>
+        <source>លាក់ឈ្មោះធាតុផ្សំ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="245"/>
+        <location filename="../src/app/maintoolbar.cpp" line="465"/>
+        <source>បង្ហាញឈ្មោះធាតុផ្សំ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="255"/>
+        <location filename="../src/app/maintoolbar.cpp" line="467"/>
+        <source>ដាក់ឈ្មោះជើង</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="258"/>
+        <location filename="../src/app/maintoolbar.cpp" line="266"/>
+        <location filename="../src/app/maintoolbar.cpp" line="469"/>
+        <source>លាក់ឈ្មោះជើង</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="268"/>
+        <location filename="../src/app/maintoolbar.cpp" line="471"/>
+        <source>បង្ហាញឈ្មោះជើង</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="277"/>
+        <source>ជើងចោល</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="281"/>
+        <location filename="../src/app/maintoolbar.cpp" line="289"/>
+        <location filename="../src/app/maintoolbar.cpp" line="475"/>
+        <source>លាក់ឈ្មោះជើងចោល</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="291"/>
+        <location filename="../src/app/maintoolbar.cpp" line="477"/>
+        <source>បង្ហាញឈ្មោះជើងចោល</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="300"/>
+        <location filename="../src/app/maintoolbar.cpp" line="479"/>
+        <source>ម៉ាស់</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="302"/>
+        <location filename="../src/app/maintoolbar.cpp" line="308"/>
+        <location filename="../src/app/maintoolbar.cpp" line="481"/>
+        <source>លាក់ឈ្មោះជើងម៉ាស់</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="310"/>
+        <location filename="../src/app/maintoolbar.cpp" line="483"/>
+        <source>បង្ហាញឈ្មោះជើងម៉ាស់</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="320"/>
+        <location filename="../src/app/maintoolbar.cpp" line="485"/>
+        <source>បង្ហាញ / លាក់ A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="337"/>
+        <location filename="../src/app/maintoolbar.cpp" line="486"/>
+        <source>បង្ហាញ / លាក់ B</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="356"/>
+        <location filename="../src/app/maintoolbar.cpp" line="487"/>
+        <source>បង្ហាញទាំងពីរ A នឹង B</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="370"/>
+        <location filename="../src/app/maintoolbar.cpp" line="488"/>
+        <source>កំណត់ការពង្រីកឡើងវិញ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="384"/>
+        <location filename="../src/app/maintoolbar.cpp" line="489"/>
+        <source>ពង្រីក</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="396"/>
+        <location filename="../src/app/maintoolbar.cpp" line="490"/>
+        <source>បង្រួម</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="410"/>
+        <location filename="../src/app/maintoolbar.cpp" line="491"/>
+        <source>កំណត់ការបង្វិលឡើងវិញ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="422"/>
+        <location filename="../src/app/maintoolbar.cpp" line="492"/>
+        <source>បង្វិលទៅស្តាំ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="435"/>
+        <location filename="../src/app/maintoolbar.cpp" line="493"/>
+        <source>បង្វិលទៅឆ្វេង</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="473"/>
+        <source>ជើងចោល</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ផ្ទាំងមេ</name>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="39"/>
+        <location filename="../src/app/mainwindow.cpp" line="242"/>
+        <source>បើក BRD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="40"/>
+        <location filename="../src/app/mainwindow.cpp" line="243"/>
+        <source>ទូរស័ព្ទ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="41"/>
+        <location filename="../src/app/mainwindow.cpp" line="244"/>
+        <source>ថេប្លេត</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="54"/>
+        <source>បើកឯកសារបត</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="54"/>
+        <source>BRD ឯកសារ (*.brd)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="100"/>
+        <location filename="../src/app/mainwindow.cpp" line="245"/>
+        <source>ទ្រនិចបង្ហាញទិស: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="104"/>
+        <location filename="../src/app/mainwindow.cpp" line="144"/>
+        <location filename="../src/app/mainwindow.cpp" line="246"/>
+        <source>Pin ឈ្មោះ: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="108"/>
+        <location filename="../src/app/mainwindow.cpp" line="145"/>
+        <location filename="../src/app/mainwindow.cpp" line="247"/>
+        <source>ឈ្មោះបច្ចេកទេស: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="150"/>
+        <source>ជើងមានទំនក់ទំនងលើផ្ទាំងទាំងពីរ.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="152"/>
+        <source>ជើង [% 1] មានទំនាក់ទំនងលើផ្ទាំងទាំងពីរ.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="159"/>
+        <source>ជើងទំនាក់ទំនងនៅចំហៀង % 1 ប៉ុណ្ណោះ.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="161"/>
+        <source>ជើង [% 1] មានទំនាក់ទំនងនៅចំហៀង % 2 ប៉ុណ្ណោះ.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="193"/>
+        <source>អាចធ្វើបច្ចុប្បន្នភាពបាន</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="195"/>
+        <source>បើចង់ធ្វើបច្ចុប្បន្នភាព!
+សូមចូលទៅ កាន់ Phoneboard &lt;a href=&quot;https://phoneboard.co&quot;&gt;website&lt;/a&gt; to download.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ការដំណើរការវឌ្ឍនភាព</name>
+    <message>
+        <location filename="../src/app/overlayprogress.cpp" line="65"/>
+        <source>កំពុងផ្ទុក %1...</source>
+        <extracomment>This text appears upside the loading progress bar.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ការដំណើរការវឌ្ឍនភាព</name>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="56"/>
+        <location filename="../src/app/overlaysettings.cpp" line="323"/>
+        <source>ស្វាគមន៍មកកាន់ Phoneboard!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="68"/>
+        <location filename="../src/app/overlaysettings.cpp" line="324"/>
+        <source>កន្លែងកម្មវិធីចាំនៅទីនេះ គឺ &lt;b&gt;free&lt;/b&gt;. To report any issues go &lt;a href=&quot;https://github.com/phoneboard/phoneboard/issues&quot;&gt;here&lt;/a&gt;.&lt;br&gt;If you find the app useful, consider supporting me on &lt;a href=&quot;https://www.patreon.com/Phoneboard&quot;&gt;Patreon&lt;/a&gt; or donate on &lt;a href=&quot;https://www.paypal.me/phoneboardapp&quot;&gt;Paypal&lt;/a&gt;.Thank you!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="90"/>
+        <location filename="../src/app/overlaysettings.cpp" line="319"/>
+        <source>ភាសា</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="120"/>
+        <location filename="../src/app/overlaysettings.cpp" line="318"/>
+        <source>បំបែកផ្ទាំងបត</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="122"/>
+        <location filename="../src/app/overlaysettings.cpp" line="320"/>
+        <source>ផ្ដេក</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="123"/>
+        <location filename="../src/app/overlaysettings.cpp" line="321"/>
+        <source>បញ្ឈរ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="142"/>
+        <location filename="../src/app/overlaysettings.cpp" line="334"/>
+        <source>គ្មានទំនាក់ទំនងពណ៌</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/ការដំណើរការវឌ្ឍនភាព.cpp" line="144"/>
+        <location filename="../src/app/ការដំណើរការវឌ្ឍនភាព.cpp" line="196"/>
+        <location filename="../src/app/ការដំណើរការវឌ្ឍនភាព.cpp" line="247"/>
+        <location filename="../src/app/ការដំណើរការវឌ្ឍនភាព.cpp" line="335"/>
+        <location filename="../src/app/ការដំណើរការវឌ្ឍនភាព.cpp" line="338"/>
+        <location filename="../src/app/ការដំណើរការវឌ្ឍនភាព.cpp" line="341"/>
+        <source>កំណត់ឡើងវិញ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="194"/>
+        <location filename="../src/app/overlaysettings.cpp" line="337"/>
+        <source>ពណ៌ម៉ាស់</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="245"/>
+        <location filename="../src/app/overlaysettings.cpp" line="340"/>
+        <source>ដំណើរការពណ៌</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OverlayWelcome</name>
+    <message>
+        <location filename="../src/app/overlaywelcome.cpp" line="53"/>
+        <source>អ្វីដែលថ្មីរបស់ Apple iOS នៅក្នុងជំនាន់ថ្មី%1!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
