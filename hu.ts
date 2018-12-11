891772c0472e72c93cd32f123b1411e0c6e59369
Guido Pola <guidopola@live.com>
2018-12-11 19:28:41 -0300

    Create hu.ts
    Add hungarian translation by Mobilguru (Daniel Katona)

---
+++ hu.ts
@@ -0,0 +1,327 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>ComponentItemModel</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="77"/>
+        <source>Net name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="77"/>
+        <source>Component</source>
+        <translation>Alkatrész</translation>
+    </message>
+</context>
+<context>
+    <name>MainToolBar</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="179"/>
+        <location filename="../src/app/maintoolbar.cpp" line="459"/>
+        <source>Search...</source>
+        <translation>Keresés...</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="231"/>
+        <location filename="../src/app/maintoolbar.cpp" line="460"/>
+        <source>Component name</source>
+        <translation>Alkatrész neve</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="235"/>
+        <location filename="../src/app/maintoolbar.cpp" line="243"/>
+        <location filename="../src/app/maintoolbar.cpp" line="463"/>
+        <source>Hide Components names</source>
+        <translation>Alkatrészek nevének elrejtése</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="245"/>
+        <location filename="../src/app/maintoolbar.cpp" line="465"/>
+        <source>Show Components names</source>
+        <translation>Alkatrészek nevének mutatása</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="255"/>
+        <location filename="../src/app/maintoolbar.cpp" line="467"/>
+        <source>Pin name</source>
+        <translation>Láb elnevezés</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="258"/>
+        <location filename="../src/app/maintoolbar.cpp" line="266"/>
+        <location filename="../src/app/maintoolbar.cpp" line="469"/>
+        <source>Hide Pins names</source>
+        <translation>Láb elnevezés elrejtése</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="268"/>
+        <location filename="../src/app/maintoolbar.cpp" line="471"/>
+        <source>Show Pins names</source>
+        <translation>Láb elnevezés mutatása</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="277"/>
+        <source>No Contact</source>
+        <translation>Üres</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="281"/>
+        <location filename="../src/app/maintoolbar.cpp" line="289"/>
+        <location filename="../src/app/maintoolbar.cpp" line="475"/>
+        <source>Hide No Contact pins</source>
+        <translation>Üres elrejtése</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="291"/>
+        <location filename="../src/app/maintoolbar.cpp" line="477"/>
+        <source>Show No Contact pins</source>
+        <translation>Üres mutatása</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="300"/>
+        <location filename="../src/app/maintoolbar.cpp" line="479"/>
+        <source>Ground</source>
+        <translation>Föld</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="302"/>
+        <location filename="../src/app/maintoolbar.cpp" line="308"/>
+        <location filename="../src/app/maintoolbar.cpp" line="481"/>
+        <source>Hide Ground contacts</source>
+        <translation>Föld csatlakozások elrejtése</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="310"/>
+        <location filename="../src/app/maintoolbar.cpp" line="483"/>
+        <source>Show Ground contacts</source>
+        <translation>Föld csatlakozások mutatása</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="320"/>
+        <location filename="../src/app/maintoolbar.cpp" line="485"/>
+        <source>Show / Hide A</source>
+        <translation>A megjelenítése / elrejtése</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="337"/>
+        <location filename="../src/app/maintoolbar.cpp" line="486"/>
+        <source>Show / Hide B</source>
+        <translation>B megjelenítése / elrejtése</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="356"/>
+        <location filename="../src/app/maintoolbar.cpp" line="487"/>
+        <source>Show both sides</source>
+        <translation>Mindkét oldal mutatása</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="370"/>
+        <location filename="../src/app/maintoolbar.cpp" line="488"/>
+        <source>Reset zoom</source>
+        <translation>Nagyítás visszaállítása</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="384"/>
+        <location filename="../src/app/maintoolbar.cpp" line="489"/>
+        <source>Zoom in</source>
+        <translation>Nagyítás</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="396"/>
+        <location filename="../src/app/maintoolbar.cpp" line="490"/>
+        <source>Zoom out</source>
+        <translation>Kicsinyítés</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="410"/>
+        <location filename="../src/app/maintoolbar.cpp" line="491"/>
+        <source>Reset rotation</source>
+        <translation>Elforgatás visszaállítása</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="422"/>
+        <location filename="../src/app/maintoolbar.cpp" line="492"/>
+        <source>Rotate right</source>
+        <translation>Elforgatás jobbra</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="435"/>
+        <location filename="../src/app/maintoolbar.cpp" line="493"/>
+        <source>Rotate left</source>
+        <translation>Elforgatás balra</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="473"/>
+        <source>No contact</source>
+        <translation>Üres</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="39"/>
+        <location filename="../src/app/mainwindow.cpp" line="242"/>
+        <source>Open BRD</source>
+        <translation>BRD megnyitás</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="40"/>
+        <location filename="../src/app/mainwindow.cpp" line="243"/>
+        <source>Phones</source>
+        <translation>Telefonok</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="41"/>
+        <location filename="../src/app/mainwindow.cpp" line="244"/>
+        <source>Tablets</source>
+        <translation>Tabletek</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="54"/>
+        <source>Open Board file</source>
+        <translation>Board file megnyitása</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="54"/>
+        <source>BRD Files (*.brd)</source>
+        <translation>BRD Fileok (*.brd)</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="100"/>
+        <location filename="../src/app/mainwindow.cpp" line="245"/>
+        <source>Cursor: %1</source>
+        <translation>Mutató: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="104"/>
+        <location filename="../src/app/mainwindow.cpp" line="144"/>
+        <location filename="../src/app/mainwindow.cpp" line="246"/>
+        <source>Pin name: %1</source>
+        <translation>Láb név: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="108"/>
+        <location filename="../src/app/mainwindow.cpp" line="145"/>
+        <location filename="../src/app/mainwindow.cpp" line="247"/>
+        <source>Net name: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="150"/>
+        <source>The pin have contact on both sides.</source>
+        <translation>Mindkét oldalon csatlakozik.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="152"/>
+        <source>The pin [%1] have contact on both sides.</source>
+        <translation>A [%1] láb mindkét oldalon csatlakozik.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="159"/>
+        <source>The pin have contact on side %1 only.</source>
+        <translation>A láb csak a következő oldalon %1 csatlakozik.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="161"/>
+        <source>The pin [%1] have contact on side %2 only.</source>
+        <translation>A [%1] láb csak a következő oldalon %2 csatlakozik.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="193"/>
+        <source>Update available!</source>
+        <translation>Frissítés elérhető!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="195"/>
+        <source>An update is available!
+Please go to the Phoneboard &lt;a href=&quot;https://phoneboard.co&quot;&gt;website&lt;/a&gt; to download.</source>
+        <translation>Új frissítés elérhető!
+Kérlek látogass el a Phoneboard&lt;a href=&quot;https://phoneboard.co&quot;&gt;weboldalra&lt;/a&gt;a letöltéshez.</translation>
+    </message>
+</context>
+<context>
+    <name>OverlayProgress</name>
+    <message>
+        <location filename="../src/app/overlayprogress.cpp" line="65"/>
+        <source>Loading %1...</source>
+        <extracomment>This text appears upside the loading progress bar.</extracomment>
+        <translation>Betöltés %1...</translation>
+    </message>
+</context>
+<context>
+    <name>OverlaySettings</name>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="56"/>
+        <location filename="../src/app/overlaysettings.cpp" line="323"/>
+        <source>Welcome to Phoneboard!</source>
+        <translation>Üdvözöl a Phoneboard!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="68"/>
+        <location filename="../src/app/overlaysettings.cpp" line="324"/>
+        <source>Remember this software is &lt;b&gt;free&lt;/b&gt;. To report any issues go &lt;a href=&quot;https://github.com/phoneboard/phoneboard/issues&quot;&gt;here&lt;/a&gt;.&lt;br&gt;If you find the app useful, consider supporting me on &lt;a href=&quot;https://www.patreon.com/Phoneboard&quot;&gt;Patreon&lt;/a&gt; or donate on &lt;a href=&quot;https://www.paypal.me/phoneboardapp&quot;&gt;Paypal&lt;/a&gt;.Thank you!</source>
+        <translation>Ne feledd! Ez a software &lt;b&gt;ingyenes&lt;/b&gt;! Esetleges problémák jelentéséhez látogass el&lt;a href=&quot;https://github.com/phoneboard/phoneboard/issues&quot;&gt;ide!&lt;/a&gt;.&lt;br&gt;Amennyiben hasznosnak találod ezt az alkalmazást, fontold meg a támogatásom &lt;a href=&quot;https://www.patreon.com/Phoneboard&quot;&gt;Patreon-on&lt;/a&gt;vagy pedig &lt;a href=&quot;https://www.paypal.me/phoneboardapp&quot;&gt;Paypa-onl&lt;/a&gt;.Köszönöm!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="90"/>
+        <location filename="../src/app/overlaysettings.cpp" line="319"/>
+        <source>Language</source>
+        <translation>Nyelv</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="120"/>
+        <location filename="../src/app/overlaysettings.cpp" line="318"/>
+        <source>Split Board</source>
+        <translation>Osztott nézet</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="122"/>
+        <location filename="../src/app/overlaysettings.cpp" line="320"/>
+        <source>Horizontal</source>
+        <translation>Vízszintes</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="123"/>
+        <location filename="../src/app/overlaysettings.cpp" line="321"/>
+        <source>Vertical</source>
+        <translation>Függőleges</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="142"/>
+        <location filename="../src/app/overlaysettings.cpp" line="334"/>
+        <source>No Contact Color</source>
+        <translation>Üres Színe</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="144"/>
+        <location filename="../src/app/overlaysettings.cpp" line="196"/>
+        <location filename="../src/app/overlaysettings.cpp" line="247"/>
+        <location filename="../src/app/overlaysettings.cpp" line="335"/>
+        <location filename="../src/app/overlaysettings.cpp" line="338"/>
+        <location filename="../src/app/overlaysettings.cpp" line="341"/>
+        <source>Reset</source>
+        <translation>Visszaállítás</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="194"/>
+        <location filename="../src/app/overlaysettings.cpp" line="337"/>
+        <source>Ground Color</source>
+        <translation>Föld színe</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="245"/>
+        <location filename="../src/app/overlaysettings.cpp" line="340"/>
+        <source>Active Color</source>
+        <translation>Kijelőlés színe</translation>
+    </message>
+</context>
+<context>
+    <name>OverlayWelcome</name>
+    <message>
+        <location filename="../src/app/overlaywelcome.cpp" line="53"/>
+        <source>What&apos;s new in v%1!</source>
+        <translation>Újdonságok a v%1 verzióban!</translation>
+    </message>
+</context>
+</TS>
