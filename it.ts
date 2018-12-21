9005fc7d2efbd235e4a76bdcd7471d670db2def0
Guido Pola <guidopola@live.com>
2018-12-20 23:11:57 -0300

    Add italian translation by Nicola Ripiccini

---
+++ it.ts
@@ -0,0 +1,327 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT" sourcelanguage="en">
+<context>
+    <name>ComponentItemModel</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="77"/>
+        <source>Net name</source>
+        <translation>Nome Linea</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="77"/>
+        <source>Component</source>
+        <translation>Componente</translation>
+    </message>
+</context>
+<context>
+    <name>MainToolBar</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="179"/>
+        <location filename="../src/app/maintoolbar.cpp" line="459"/>
+        <source>Search...</source>
+        <translation>Cerca...</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="231"/>
+        <location filename="../src/app/maintoolbar.cpp" line="460"/>
+        <source>Component name</source>
+        <translation>Nome Componente</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="235"/>
+        <location filename="../src/app/maintoolbar.cpp" line="243"/>
+        <location filename="../src/app/maintoolbar.cpp" line="463"/>
+        <source>Hide Components names</source>
+        <translation>Nascondi il nome dei componenti</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="245"/>
+        <location filename="../src/app/maintoolbar.cpp" line="465"/>
+        <source>Show Components names</source>
+        <translation>Mostra il nome dei componenti</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="255"/>
+        <location filename="../src/app/maintoolbar.cpp" line="467"/>
+        <source>Pin name</source>
+        <translation>Nome Piazzola (Pin)</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="258"/>
+        <location filename="../src/app/maintoolbar.cpp" line="266"/>
+        <location filename="../src/app/maintoolbar.cpp" line="469"/>
+        <source>Hide Pins names</source>
+        <translation>Nascondi il nome delle Piazzole</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="268"/>
+        <location filename="../src/app/maintoolbar.cpp" line="471"/>
+        <source>Show Pins names</source>
+        <translation>Mostra il nome delle piazzole</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="277"/>
+        <source>No Contact</source>
+        <translation>Non Collegate (NC)</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="281"/>
+        <location filename="../src/app/maintoolbar.cpp" line="289"/>
+        <location filename="../src/app/maintoolbar.cpp" line="475"/>
+        <source>Hide No Contact pins</source>
+        <translation>Nascondi Piazzole Non Collegate (NC)</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="291"/>
+        <location filename="../src/app/maintoolbar.cpp" line="477"/>
+        <source>Show No Contact pins</source>
+        <translation>Mostra Piazzole Non Collegate (NC)</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="300"/>
+        <location filename="../src/app/maintoolbar.cpp" line="479"/>
+        <source>Ground</source>
+        <translation>Massa</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="302"/>
+        <location filename="../src/app/maintoolbar.cpp" line="308"/>
+        <location filename="../src/app/maintoolbar.cpp" line="481"/>
+        <source>Hide Ground contacts</source>
+        <translation>Nascondi Piazzole a Massa</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="310"/>
+        <location filename="../src/app/maintoolbar.cpp" line="483"/>
+        <source>Show Ground contacts</source>
+        <translation>Mostra Piazzole a Massa</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="320"/>
+        <location filename="../src/app/maintoolbar.cpp" line="485"/>
+        <source>Show / Hide A</source>
+        <translation>Mostra / Nascondi A</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="337"/>
+        <location filename="../src/app/maintoolbar.cpp" line="486"/>
+        <source>Show / Hide B</source>
+        <translation>Mostra / Nascondi B</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="356"/>
+        <location filename="../src/app/maintoolbar.cpp" line="487"/>
+        <source>Show both sides</source>
+        <translation>Mostra entrambi i lati</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="370"/>
+        <location filename="../src/app/maintoolbar.cpp" line="488"/>
+        <source>Reset zoom</source>
+        <translation>Reset Zoom</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="384"/>
+        <location filename="../src/app/maintoolbar.cpp" line="489"/>
+        <source>Zoom in</source>
+        <translation>Zoom in</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="396"/>
+        <location filename="../src/app/maintoolbar.cpp" line="490"/>
+        <source>Zoom out</source>
+        <translation>Zoom out</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="410"/>
+        <location filename="../src/app/maintoolbar.cpp" line="491"/>
+        <source>Reset rotation</source>
+        <translation>Reset Rotazione</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="422"/>
+        <location filename="../src/app/maintoolbar.cpp" line="492"/>
+        <source>Rotate right</source>
+        <translation>Ruota a destra</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="435"/>
+        <location filename="../src/app/maintoolbar.cpp" line="493"/>
+        <source>Rotate left</source>
+        <translation>Ruota a sinistra</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="473"/>
+        <source>No contact</source>
+        <translation>Non Collegate (NC)</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="39"/>
+        <location filename="../src/app/mainwindow.cpp" line="242"/>
+        <source>Open BRD</source>
+        <translation>Apri BRD</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="40"/>
+        <location filename="../src/app/mainwindow.cpp" line="243"/>
+        <source>Phones</source>
+        <translation>Telefoni</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="41"/>
+        <location filename="../src/app/mainwindow.cpp" line="244"/>
+        <source>Tablets</source>
+        <translation>Tablets</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="54"/>
+        <source>Open Board file</source>
+        <translation>Apri Board File</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="54"/>
+        <source>BRD Files (*.brd)</source>
+        <translation>File BRD (*.brd)</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="100"/>
+        <location filename="../src/app/mainwindow.cpp" line="245"/>
+        <source>Cursor: %1</source>
+        <translation>Cursore: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="104"/>
+        <location filename="../src/app/mainwindow.cpp" line="144"/>
+        <location filename="../src/app/mainwindow.cpp" line="246"/>
+        <source>Pin name: %1</source>
+        <translation>Nome del Pin: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="108"/>
+        <location filename="../src/app/mainwindow.cpp" line="145"/>
+        <location filename="../src/app/mainwindow.cpp" line="247"/>
+        <source>Net name: %1</source>
+        <translation>Nome Linea: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="150"/>
+        <source>The pin have contact on both sides.</source>
+        <translation>La Piazzola ha un contatto su entrambi i lati.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="152"/>
+        <source>The pin [%1] have contact on both sides.</source>
+        <translation>La piazzola[%1] ha un contatto su entrambi i lati.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="159"/>
+        <source>The pin have contact on side %1 only.</source>
+        <translation>La piazzola ha il contatto solo sul lato%1.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="161"/>
+        <source>The pin [%1] have contact on side %2 only.</source>
+        <translation>La piazzola [%1] ha solo il contatto sul lato%2.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="193"/>
+        <source>Update available!</source>
+        <translation>Aggiornamento Disponibile!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="195"/>
+        <source>An update is available!
+Please go to the Phoneboard &lt;a href=&quot;https://phoneboard.co&quot;&gt;website&lt;/a&gt; to download.</source>
+        <translation>Un aggiornamento è disponibile!
+Si prega di andare sul sito di &lt;a href=&quot;https://phoneboard.co&quot;&gt;PhoneBoard&lt;/a&gt;per scaricarlo.</translation>
+    </message>
+</context>
+<context>
+    <name>OverlayProgress</name>
+    <message>
+        <location filename="../src/app/overlayprogress.cpp" line="65"/>
+        <source>Loading %1...</source>
+        <extracomment>This text appears upside the loading progress bar.</extracomment>
+        <translation>Caricamento %1...</translation>
+    </message>
+</context>
+<context>
+    <name>OverlaySettings</name>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="56"/>
+        <location filename="../src/app/overlaysettings.cpp" line="323"/>
+        <source>Welcome to Phoneboard!</source>
+        <translation>Benvenuto in Phoneboard!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="68"/>
+        <location filename="../src/app/overlaysettings.cpp" line="324"/>
+        <source>Remember this software is &lt;b&gt;free&lt;/b&gt;. To report any issues go &lt;a href=&quot;https://github.com/phoneboard/phoneboard/issues&quot;&gt;here&lt;/a&gt;.&lt;br&gt;If you find the app useful, consider supporting me on &lt;a href=&quot;https://www.patreon.com/Phoneboard&quot;&gt;Patreon&lt;/a&gt; or donate on &lt;a href=&quot;https://www.paypal.me/phoneboardapp&quot;&gt;Paypal&lt;/a&gt;.Thank you!</source>
+        <translation>Ricorda questo software è&lt;b&gt;gratuito&lt;/b&gt;. Per segnalare qualsiasi problema vai &lt;a href=&quot;https://github.com/phoneboard/phoneboard/issues&quot;&gt;qui&lt;/a&gt;.&lt;b&gt;Se trovi utile l&apos;app, considera di sostenermi su &lt;a href=&quot;https://www.patreon.com/Phoneboard&quot;&gt;Patreon&lt;/a&gt; o di donare su &lt;a href=&quot;https://www.paypal.me/phoneboardapp&quot;&gt;Paypal&lt;/a&gt; Grazie!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="90"/>
+        <location filename="../src/app/overlaysettings.cpp" line="319"/>
+        <source>Language</source>
+        <translation>Lingua</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="120"/>
+        <location filename="../src/app/overlaysettings.cpp" line="318"/>
+        <source>Split Board</source>
+        <translation>Dividi Scheda Madre (PCB)</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="122"/>
+        <location filename="../src/app/overlaysettings.cpp" line="320"/>
+        <source>Horizontal</source>
+        <translation>Orizzontale</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="123"/>
+        <location filename="../src/app/overlaysettings.cpp" line="321"/>
+        <source>Vertical</source>
+        <translation>Verticale</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="142"/>
+        <location filename="../src/app/overlaysettings.cpp" line="334"/>
+        <source>No Contact Color</source>
+        <translation>Colore Piazzole Non Collegate (NC)</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="144"/>
+        <location filename="../src/app/overlaysettings.cpp" line="196"/>
+        <location filename="../src/app/overlaysettings.cpp" line="247"/>
+        <location filename="../src/app/overlaysettings.cpp" line="335"/>
+        <location filename="../src/app/overlaysettings.cpp" line="338"/>
+        <location filename="../src/app/overlaysettings.cpp" line="341"/>
+        <source>Reset</source>
+        <translation>Reset</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="194"/>
+        <location filename="../src/app/overlaysettings.cpp" line="337"/>
+        <source>Ground Color</source>
+        <translation>Colore Massa</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="245"/>
+        <location filename="../src/app/overlaysettings.cpp" line="340"/>
+        <source>Active Color</source>
+        <translation>Colore Piazzole Collegate (Attive)</translation>
+    </message>
+</context>
+<context>
+    <name>OverlayWelcome</name>
+    <message>
+        <location filename="../src/app/overlaywelcome.cpp" line="53"/>
+        <source>What&apos;s new in v%1!</source>
+        <translation>Cosa c&apos;è di nuovo nella v%1!</translation>
+    </message>
+</context>
+</TS>
