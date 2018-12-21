c4b0cb4016179d7adf21c8fd655a7963626ac31d
Guido Pola <guidopola@live.com>
2018-12-20 23:11:31 -0300

    Add finnish translation by Ruuben Vuolasranta

---
+++ fi.ts
@@ -0,0 +1,327 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi_FI">
+<context>
+    <name>ComponentItemModel</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="77"/>
+        <source>Net name</source>
+        <translation>Piirisarjan nimi</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="77"/>
+        <source>Component</source>
+        <translation>Komponentti</translation>
+    </message>
+</context>
+<context>
+    <name>MainToolBar</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="179"/>
+        <location filename="../src/app/maintoolbar.cpp" line="459"/>
+        <source>Search...</source>
+        <translation>Etsi...</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="231"/>
+        <location filename="../src/app/maintoolbar.cpp" line="460"/>
+        <source>Component name</source>
+        <translation>Komponentin nimi</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="235"/>
+        <location filename="../src/app/maintoolbar.cpp" line="243"/>
+        <location filename="../src/app/maintoolbar.cpp" line="463"/>
+        <source>Hide Components names</source>
+        <translation>Piilota komponenttien nimet</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="245"/>
+        <location filename="../src/app/maintoolbar.cpp" line="465"/>
+        <source>Show Components names</source>
+        <translation>Näytä komponenttien nimet</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="255"/>
+        <location filename="../src/app/maintoolbar.cpp" line="467"/>
+        <source>Pin name</source>
+        <translation>Pinnin nimi</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="258"/>
+        <location filename="../src/app/maintoolbar.cpp" line="266"/>
+        <location filename="../src/app/maintoolbar.cpp" line="469"/>
+        <source>Hide Pins names</source>
+        <translation>Piilota pinnien nimet</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="268"/>
+        <location filename="../src/app/maintoolbar.cpp" line="471"/>
+        <source>Show Pins names</source>
+        <translation>Näytä pinnien nimet</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="277"/>
+        <source>No Contact</source>
+        <translation>Tyhjät kontaktit</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="281"/>
+        <location filename="../src/app/maintoolbar.cpp" line="289"/>
+        <location filename="../src/app/maintoolbar.cpp" line="475"/>
+        <source>Hide No Contact pins</source>
+        <translation>Piilota tyhjät kontaktit</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="291"/>
+        <location filename="../src/app/maintoolbar.cpp" line="477"/>
+        <source>Show No Contact pins</source>
+        <translation>Näytä tyhjät kontaktit</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="300"/>
+        <location filename="../src/app/maintoolbar.cpp" line="479"/>
+        <source>Ground</source>
+        <translation>Maadoitus</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="302"/>
+        <location filename="../src/app/maintoolbar.cpp" line="308"/>
+        <location filename="../src/app/maintoolbar.cpp" line="481"/>
+        <source>Hide Ground contacts</source>
+        <translation>Piilota maadoitus</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="310"/>
+        <location filename="../src/app/maintoolbar.cpp" line="483"/>
+        <source>Show Ground contacts</source>
+        <translation>Näytä maadoitus</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="320"/>
+        <location filename="../src/app/maintoolbar.cpp" line="485"/>
+        <source>Show / Hide A</source>
+        <translation>Näytä /Piilota A</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="337"/>
+        <location filename="../src/app/maintoolbar.cpp" line="486"/>
+        <source>Show / Hide B</source>
+        <translation>Näytä /Piilota B</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="356"/>
+        <location filename="../src/app/maintoolbar.cpp" line="487"/>
+        <source>Show both sides</source>
+        <translation>Näytä molemmat puolet</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="370"/>
+        <location filename="../src/app/maintoolbar.cpp" line="488"/>
+        <source>Reset zoom</source>
+        <translation>Palauta.zoomaus</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="384"/>
+        <location filename="../src/app/maintoolbar.cpp" line="489"/>
+        <source>Zoom in</source>
+        <translation>Zoomaa sisään</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="396"/>
+        <location filename="../src/app/maintoolbar.cpp" line="490"/>
+        <source>Zoom out</source>
+        <translation>Zoomaa ulos</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="410"/>
+        <location filename="../src/app/maintoolbar.cpp" line="491"/>
+        <source>Reset rotation</source>
+        <translation>Palauta.asento</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="422"/>
+        <location filename="../src/app/maintoolbar.cpp" line="492"/>
+        <source>Rotate right</source>
+        <translation>Käännä myötäpäivään</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="435"/>
+        <location filename="../src/app/maintoolbar.cpp" line="493"/>
+        <source>Rotate left</source>
+        <translation>Käännä vastapäivään</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="473"/>
+        <source>No contact</source>
+        <translation>Ei kontaktia</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="39"/>
+        <location filename="../src/app/mainwindow.cpp" line="242"/>
+        <source>Open BRD</source>
+        <translation>Avaa BRD</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="40"/>
+        <location filename="../src/app/mainwindow.cpp" line="243"/>
+        <source>Phones</source>
+        <translation>Puhelimet</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="41"/>
+        <location filename="../src/app/mainwindow.cpp" line="244"/>
+        <source>Tablets</source>
+        <translation>Tabletit</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="54"/>
+        <source>Open Board file</source>
+        <translation>Avaa Board-tiedosto</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="54"/>
+        <source>BRD Files (*.brd)</source>
+        <translation>BDR-tiedostot(*.brd)</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="100"/>
+        <location filename="../src/app/mainwindow.cpp" line="245"/>
+        <source>Cursor: %1</source>
+        <translation>Kursori: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="104"/>
+        <location filename="../src/app/mainwindow.cpp" line="144"/>
+        <location filename="../src/app/mainwindow.cpp" line="246"/>
+        <source>Pin name: %1</source>
+        <translation>Pinnin nimi: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="108"/>
+        <location filename="../src/app/mainwindow.cpp" line="145"/>
+        <location filename="../src/app/mainwindow.cpp" line="247"/>
+        <source>Net name: %1</source>
+        <translation>Piirisarjan nimi: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="150"/>
+        <source>The pin have contact on both sides.</source>
+        <translation>Pinnillä on yhteys molemmilla puolilla.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="152"/>
+        <source>The pin [%1] have contact on both sides.</source>
+        <translation>Pinnillä [%1] on kontakti molemmilla puolilla.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="159"/>
+        <source>The pin have contact on side %1 only.</source>
+        <translation>Pinnillä on kontakti vain %1  puolella.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="161"/>
+        <source>The pin [%1] have contact on side %2 only.</source>
+        <translation>Pinnillä [%1]on kontakti vain %2 puolella.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="193"/>
+        <source>Update available!</source>
+        <translation>Uusi päivitys on saatavilla!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="195"/>
+        <source>An update is available!
+Please go to the Phoneboard &lt;a href=&quot;https://phoneboard.co&quot;&gt;website&lt;/a&gt; to download.</source>
+        <translation>Uusi päivitys on saatavilla!
+Mene osoitteeseen Phoneboard &lt;a href=&quot;https://phoneboards.co&quot;&gt;website&lt;/a&gt; ja lataa uusin päivitys.</translation>
+    </message>
+</context>
+<context>
+    <name>OverlayProgress</name>
+    <message>
+        <location filename="../src/app/overlayprogress.cpp" line="65"/>
+        <source>Loading %1...</source>
+        <extracomment>This text appears upside the loading progress bar.</extracomment>
+        <translation>Ladataan %1...</translation>
+    </message>
+</context>
+<context>
+    <name>OverlaySettings</name>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="56"/>
+        <location filename="../src/app/overlaysettings.cpp" line="323"/>
+        <source>Welcome to Phoneboard!</source>
+        <translation>Tervetuloa käyttämään Phoneboardia!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="68"/>
+        <location filename="../src/app/overlaysettings.cpp" line="324"/>
+        <source>Remember this software is &lt;b&gt;free&lt;/b&gt;. To report any issues go &lt;a href=&quot;https://github.com/phoneboard/phoneboard/issues&quot;&gt;here&lt;/a&gt;.&lt;br&gt;If you find the app useful, consider supporting me on &lt;a href=&quot;https://www.patreon.com/Phoneboard&quot;&gt;Patreon&lt;/a&gt; or donate on &lt;a href=&quot;https://www.paypal.me/phoneboardapp&quot;&gt;Paypal&lt;/a&gt;.Thank you!</source>
+        <translation>Muistathan, että tämä ohjelma on &lt;b&gt;ilmainen&lt;/b&gt;. Ilmoitaaksesi ongelmasta ,siirry &lt;a href=&quot;https://github.com/phoneboard/phoneboard/issues&quot;&gt;tänne&lt;/a&gt;.&lt;br&gt;Jos pidät ohjelmasta, harkitse tukemistani palvelussa &lt;a href=&quot;https://www.patreon.com/Phoneboard&quot;&gt;Patreon&lt;/a&gt; tai lahjoittamalla &lt;a href=&quot;https://www.paypal.me/phoneboardapp&quot;&gt;Paypal&lt;/a&gt;.Kiitos!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="90"/>
+        <location filename="../src/app/overlaysettings.cpp" line="319"/>
+        <source>Language</source>
+        <translation>Kieli</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="120"/>
+        <location filename="../src/app/overlaysettings.cpp" line="318"/>
+        <source>Split Board</source>
+        <translation>Jaa emolevyt</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="122"/>
+        <location filename="../src/app/overlaysettings.cpp" line="320"/>
+        <source>Horizontal</source>
+        <translation>Vaakasuoraan</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="123"/>
+        <location filename="../src/app/overlaysettings.cpp" line="321"/>
+        <source>Vertical</source>
+        <translation>Pystysuoraan</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="142"/>
+        <location filename="../src/app/overlaysettings.cpp" line="334"/>
+        <source>No Contact Color</source>
+        <translation>Tyhjien kontaktien väri</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="144"/>
+        <location filename="../src/app/overlaysettings.cpp" line="196"/>
+        <location filename="../src/app/overlaysettings.cpp" line="247"/>
+        <location filename="../src/app/overlaysettings.cpp" line="335"/>
+        <location filename="../src/app/overlaysettings.cpp" line="338"/>
+        <location filename="../src/app/overlaysettings.cpp" line="341"/>
+        <source>Reset</source>
+        <translation>Oletusasetus</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="194"/>
+        <location filename="../src/app/overlaysettings.cpp" line="337"/>
+        <source>Ground Color</source>
+        <translation>Maadoituksen väri</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="245"/>
+        <location filename="../src/app/overlaysettings.cpp" line="340"/>
+        <source>Active Color</source>
+        <translation>Valitun väri</translation>
+    </message>
+</context>
+<context>
+    <name>OverlayWelcome</name>
+    <message>
+        <location filename="../src/app/overlaywelcome.cpp" line="53"/>
+        <source>What&apos;s new in v%1!</source>
+        <translation>Mitä uutta v%1:ssa!</translation>
+    </message>
+</context>
+</TS>
