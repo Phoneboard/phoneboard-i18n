4740495b36de81b38c62f73aacc28c215f172bb2
deecoy1337 <47360668+deecoy1337@users.noreply.github.com>
2019-02-05 17:33:55 +0100

    Add files via upload

---
+++ sl.ts
@@ -0,0 +1,407 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sl_SI" sourcelanguage="en">
+<context>
+    <name>ComponentItemModel</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="82"/>
+        <source>Net name</source>
+        <translation>Ime mreže</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="82"/>
+        <source>Component</source>
+        <translation>Komponenta</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="87"/>
+        <source>Both</source>
+        <translation>Obe</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="89"/>
+        <source>Side A</source>
+        <translation>Stran A</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="91"/>
+        <source>Side B</source>
+        <translation>Stran B</translation>
+    </message>
+</context>
+<context>
+    <name>MainToolBar</name>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="157"/>
+        <location filename="../src/app/maintoolbar.cpp" line="402"/>
+        <source>Search...</source>
+        <translation>Iskanje...</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="195"/>
+        <location filename="../src/app/maintoolbar.cpp" line="403"/>
+        <source>Component name</source>
+        <translation>Ime komponente</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="198"/>
+        <location filename="../src/app/maintoolbar.cpp" line="203"/>
+        <location filename="../src/app/maintoolbar.cpp" line="406"/>
+        <source>Hide Components names</source>
+        <translation>Skrij imena komponent</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="205"/>
+        <location filename="../src/app/maintoolbar.cpp" line="408"/>
+        <source>Show Components names</source>
+        <translation>Prikaži imena komponent</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="215"/>
+        <location filename="../src/app/maintoolbar.cpp" line="410"/>
+        <source>Pin name</source>
+        <translation>Ime pina</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="218"/>
+        <location filename="../src/app/maintoolbar.cpp" line="223"/>
+        <location filename="../src/app/maintoolbar.cpp" line="412"/>
+        <source>Hide Pins names</source>
+        <translation>Skrij imena pinov</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="225"/>
+        <location filename="../src/app/maintoolbar.cpp" line="414"/>
+        <source>Show Pins names</source>
+        <translation>Prikaži imena pinov</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="234"/>
+        <location filename="../src/app/maintoolbar.cpp" line="416"/>
+        <source>No Connection</source>
+        <translation>Brez povezave</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="238"/>
+        <location filename="../src/app/maintoolbar.cpp" line="242"/>
+        <location filename="../src/app/maintoolbar.cpp" line="418"/>
+        <source>Hide No Connection pins</source>
+        <translation>Skrij pine brez povezave</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="244"/>
+        <location filename="../src/app/maintoolbar.cpp" line="420"/>
+        <source>Show No Connection pins</source>
+        <translation>Prikaži pine brez povezave</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="253"/>
+        <location filename="../src/app/maintoolbar.cpp" line="422"/>
+        <source>Ground</source>
+        <translation>Ground</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="255"/>
+        <location filename="../src/app/maintoolbar.cpp" line="259"/>
+        <location filename="../src/app/maintoolbar.cpp" line="424"/>
+        <source>Hide Ground contacts</source>
+        <translation>Prikaži ground kontakte</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="261"/>
+        <location filename="../src/app/maintoolbar.cpp" line="426"/>
+        <source>Show Ground contacts</source>
+        <translation>Skrij ground kontakte</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="269"/>
+        <source>Diode Values</source>
+        <translation>Diode vrednosti</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="271"/>
+        <location filename="../src/app/maintoolbar.cpp" line="278"/>
+        <source>Show Diode Values</source>
+        <translation>Prikaži diode vrednosti</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="276"/>
+        <source>Hide Diode Values</source>
+        <translation>Skrij diode vrednosti</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="287"/>
+        <location filename="../src/app/maintoolbar.cpp" line="428"/>
+        <source>Show / Hide A</source>
+        <translation>Prikaži / skrij A</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="300"/>
+        <location filename="../src/app/maintoolbar.cpp" line="429"/>
+        <source>Show / Hide B</source>
+        <translation>Prikaži / skrij B</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="314"/>
+        <location filename="../src/app/maintoolbar.cpp" line="430"/>
+        <source>Show both sides</source>
+        <translation>Prikaži obe strani</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="326"/>
+        <location filename="../src/app/maintoolbar.cpp" line="431"/>
+        <source>Reset zoom</source>
+        <translation>Ponastavi povezavo</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="338"/>
+        <location filename="../src/app/maintoolbar.cpp" line="432"/>
+        <source>Zoom in</source>
+        <translation>Povečaj</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="348"/>
+        <location filename="../src/app/maintoolbar.cpp" line="433"/>
+        <source>Zoom out</source>
+        <translation>Pomanjšaj</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="360"/>
+        <location filename="../src/app/maintoolbar.cpp" line="434"/>
+        <source>Reset rotation</source>
+        <translation>Ponastavi zasuk</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="371"/>
+        <location filename="../src/app/maintoolbar.cpp" line="435"/>
+        <source>Rotate right</source>
+        <translation>Zasuči desno</translation>
+    </message>
+    <message>
+        <location filename="../src/app/maintoolbar.cpp" line="382"/>
+        <location filename="../src/app/maintoolbar.cpp" line="436"/>
+        <source>Rotate left</source>
+        <translation>Zasuči levo</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="42"/>
+        <location filename="../src/app/mainwindow.cpp" line="309"/>
+        <source>Open BRD</source>
+        <translation>Odpri BRD</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="43"/>
+        <location filename="../src/app/mainwindow.cpp" line="310"/>
+        <source>Phones</source>
+        <translation>Telefoni</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="44"/>
+        <location filename="../src/app/mainwindow.cpp" line="311"/>
+        <source>Tablets</source>
+        <translation>Tablice</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="45"/>
+        <source>Tools</source>
+        <translation>Orodja</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="59"/>
+        <source>Open Board file</source>
+        <translation>Odpri Board datoteko</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="59"/>
+        <source>BRD Files (*.brd)</source>
+        <translation>BRD datoteke (*.brd)</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="119"/>
+        <source>Side A</source>
+        <translation>Stran A</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="121"/>
+        <source>Side B</source>
+        <translation>Stran B</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="133"/>
+        <location filename="../src/app/mainwindow.cpp" line="173"/>
+        <location filename="../src/app/mainwindow.cpp" line="312"/>
+        <source>Cursor: %1</source>
+        <translation>Kazalec: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="137"/>
+        <location filename="../src/app/mainwindow.cpp" line="178"/>
+        <location filename="../src/app/mainwindow.cpp" line="313"/>
+        <source>Pin name: %1</source>
+        <translation>Ime pina: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="141"/>
+        <location filename="../src/app/mainwindow.cpp" line="179"/>
+        <location filename="../src/app/mainwindow.cpp" line="314"/>
+        <source>Net name: %1</source>
+        <translation>Ime mreže: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="184"/>
+        <source>The pin have contact on both sides.</source>
+        <translation>Pin ima kontakt na obeh straneh.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="186"/>
+        <source>The pin [%1] have contact on both sides.</source>
+        <translation>Pin  [%1] ima kontakt na obeh straneh.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="193"/>
+        <source>The pin have contact on side %1 only.</source>
+        <translation>Pin ima kontakt samo na strani %1.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="195"/>
+        <source>The pin [%1] have contact on side %2 only.</source>
+        <translation>Pin [%1] ima kontakt samo na strani %2.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="226"/>
+        <source>Update available!</source>
+        <translation>Posodobitev je na voljo!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/mainwindow.cpp" line="229"/>
+        <source>An update is available!
+Please go to the Phoneboard &lt;a href=&quot;https://phoneboard.co&quot;&gt;website&lt;/a&gt; to download.</source>
+        <translation>Nova posodobitev je na voljo!
+Prosimo obiščite Phoneboard &lt;a href=&quot;https://phoneboard.co&quot;&gt;spletno stran&lt;/a&gt; za prenos.</translation>
+    </message>
+</context>
+<context>
+    <name>OverlayProgress</name>
+    <message>
+        <location filename="../src/app/overlayprogress.cpp" line="44"/>
+        <source>Loading %1...</source>
+        <extracomment>This text appears upside the loading progress bar.</extracomment>
+        <translation>Nalagam %1...</translation>
+    </message>
+</context>
+<context>
+    <name>OverlaySettings</name>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="125"/>
+        <location filename="../src/app/overlaysettings.cpp" line="512"/>
+        <source>Welcome to Phoneboard!</source>
+        <translation>Prozdravljeni v Phoneboardu!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="135"/>
+        <location filename="../src/app/overlaysettings.cpp" line="513"/>
+        <source>Remember this software is &lt;b&gt;free&lt;/b&gt;. To report any issues go &lt;a href=&quot;%1&quot;&gt;here&lt;/a&gt; or contact me at &lt;a href=&quot;%2&quot;&gt;Facebook&lt;/a&gt;.&lt;br&gt;If you find the app useful, consider supporting me on &lt;a href=&quot;%3&quot;&gt;Patreon&lt;/a&gt; or donate on &lt;a href=&quot;%4&quot;&gt;Paypal&lt;/a&gt; or &lt;a href=&quot;%5&quot;&gt;Bitcoin&lt;/a&gt;.&lt;br&gt;Thank you!</source>
+        <translation>Ne pozabite, da je program &lt;b&gt;brezplačen&lt;/b&gt;. Za prijavo napak kliknite &lt;a href=&quot;%1&quot;&gt;tukaj&lt;/a&gt; ali me obiščite na &lt;a href=&quot;%2&quot;&gt;Facebook-u&lt;/a&gt;.&lt;br&gt;Če vam je aplikacija všeč, me lahko podprete na &lt;a href=&quot;%3&quot;&gt;Patreonu&lt;/a&gt; ali donirate na &lt;a href=&quot;%4&quot;&gt;Paypal&lt;/a&gt; ali &lt;a href=&quot;%5&quot;&gt;Bitcoin&lt;/a&gt;.&lt;br&gt;Hvala!</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="155"/>
+        <location filename="../src/app/overlaysettings.cpp" line="525"/>
+        <source>Translators:</source>
+        <translation>Prevajalci:</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="208"/>
+        <location filename="../src/app/overlaysettings.cpp" line="508"/>
+        <source>Language</source>
+        <translation>Jezik</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="241"/>
+        <location filename="../src/app/overlaysettings.cpp" line="507"/>
+        <source>Split Board</source>
+        <translation>Razdeli ploščo</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="243"/>
+        <location filename="../src/app/overlaysettings.cpp" line="509"/>
+        <source>Horizontal</source>
+        <translation>Vodoravno</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="244"/>
+        <location filename="../src/app/overlaysettings.cpp" line="510"/>
+        <source>Vertical</source>
+        <translation>Navpično</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="260"/>
+        <location filename="../src/app/overlaysettings.cpp" line="526"/>
+        <source>No Connection Color</source>
+        <translation>Barva brez povezave</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="421"/>
+        <location filename="../src/app/overlaysettings.cpp" line="532"/>
+        <source>Dark Mode</source>
+        <translation>Temni način</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="262"/>
+        <location filename="../src/app/overlaysettings.cpp" line="316"/>
+        <location filename="../src/app/overlaysettings.cpp" line="369"/>
+        <location filename="../src/app/overlaysettings.cpp" line="527"/>
+        <location filename="../src/app/overlaysettings.cpp" line="529"/>
+        <location filename="../src/app/overlaysettings.cpp" line="531"/>
+        <source>Reset</source>
+        <translation>Ponastavi</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="94"/>
+        <source>About</source>
+        <translation>O nas</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="97"/>
+        <source>Changelog</source>
+        <translation>Spremembe</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="95"/>
+        <location filename="../src/app/overlaysettings.cpp" line="199"/>
+        <source>Settings</source>
+        <translation>Nastavitve</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="165"/>
+        <source>Arabic by: Haythem hamdi.&lt;br&gt;Cambodian by: IC Fix Sothy.&lt;br&gt;Croatian by: Saša Valić.&lt;br&gt;Danish by: Olgierd Nowakowski.&lt;br&gt;Finnish by: Ruuben Vuolasranta.&lt;br&gt;French by: Occaz&apos;out.&lt;br&gt;German by: David Schmidt.&lt;br&gt;Hungarian by: Dani Bá.&lt;br&gt;Italian by: Nicola Ripiccini.&lt;br&gt;Lithuanian by: VeryFastSnail.&lt;br&gt;Norwegian Bokmål by: Martin Refseth.&lt;br&gt;Polish by: Jakub Werwiński.&lt;br&gt;Russian by: Олег Агекян.&lt;br&gt;</source>
+        <translation>Arabic by: Haythem hamdi.&lt;br&gt;Cambodian by: IC Fix Sothy.&lt;br&gt;Croatian by: Saša Valić.&lt;br&gt;Danish by: Olgierd Nowakowski.&lt;br&gt;Finnish by: Ruuben Vuolasranta.&lt;br&gt;French by: Occaz&apos;out.&lt;br&gt;German by: David Schmidt.&lt;br&gt;Hungarian by: Dani Bá.&lt;br&gt;Italian by: Nicola Ripiccini.&lt;br&gt;Lithuanian by: VeryFastSnail.&lt;br&gt;Norwegian Bokmål by: Martin Refseth.&lt;br&gt;Polish by: Jakub Werwiński.&lt;br&gt;Russian by: Олег Агекян.&lt;br&gt;Slovenian by: Jernej Klarič.&lt;br&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="314"/>
+        <location filename="../src/app/overlaysettings.cpp" line="528"/>
+        <source>Ground Color</source>
+        <translation>Barva grounda</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="367"/>
+        <location filename="../src/app/overlaysettings.cpp" line="530"/>
+        <source>Active Color</source>
+        <translation>Barva aktivnega</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="434"/>
+        <location filename="../src/app/overlaysettings.cpp" line="533"/>
+        <source>Antialiasing</source>
+        <translation>Antialiasing</translation>
+    </message>
+    <message>
+        <location filename="../src/app/overlaysettings.cpp" line="437"/>
+        <location filename="../src/app/overlaysettings.cpp" line="535"/>
+        <source>Antialiasing produces a better drawing quality</source>
+        <translation>Antialiasing produces a better drawing quality</translation>
+    </message>
+</context>
+</TS>
